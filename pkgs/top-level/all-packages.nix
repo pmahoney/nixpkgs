fbf6c43c9350182f12ee816a2c2fc756bb6fdc66
Patrick Mahoney <pat@polycrystal.org>
2015-01-24 20:09:14 -0600

    Remove faust.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12507,11 +12507,6 @@ let
 
   fakenes = callPackage ../misc/emulators/fakenes { };
 
-
-  faust = callPackage ../applications/audio/faust { };
-
-  faust-compiler = callPackage ../applications/audio/faust-compiler { };
-
   fceux = callPackage ../misc/emulators/fceux { };
 
   foldingathome = callPackage ../misc/foldingathome { };
